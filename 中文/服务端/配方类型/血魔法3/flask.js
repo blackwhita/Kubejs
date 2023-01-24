dee4eee67a6af574219b19d0141a4916cfb50e7a
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 21:32:38 +0800

    Create flask.js

---
+++ 中文/服务端/配方类型/血魔法3/flask.js
@@ -0,0 +1,14 @@
+//
+{
+  "type": "bloodmagic:flask_$(type)",
+  "input": [
+    {
+      "item": "item"
+    }
+  ],
+  "syphon": value,
+  "ticks": value,
+  "upgradeLevel": value,
+  /"effect": "minecraft:regeneration",/
+  /"lengthDurMod": 2.6667/
+}
