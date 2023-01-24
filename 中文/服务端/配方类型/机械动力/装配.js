43834064297c27fac57b9db31d6c6c9498c1aa89
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 18:32:22 +0800

    Create 装配.js

---
+++ 中文/服务端/配方类型/机械动力/装配.js
@@ -0,0 +1,20 @@
+//{
+  "type": "create:compacting",
+  "ingredients": [
+    {
+      "item": "item"
+    },
+    {
+      "fluid": "fluid",
+      "nbt": {},
+      "amount": value
+    }
+  ],
+  "results": [
+    {
+      "item": "item"
+    }
+  ]
+}
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+event.recipes.createCompacting(output[], input[])
