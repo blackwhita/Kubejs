3cdfee339a469a3fe714444661fad0ab320f3d16
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 19:22:31 +0800

    Update 金属冲压机.js

--- 中文/服务端/配方类型/沉浸工程/金属冲压机.js
+++ 中文/服务端/配方类型/沉浸工程/金属冲压机.js
@@ -12,3 +12,18 @@ onEvent('recipes', (event) => {
         event.recipes.immersiveengineering.metal_press(recipe.output, recipe.input, recipe.mold).id(recipe.id);
     });
 });
+//
+{
+    "type":"immersiveengineering:metal_press",
+    "mold":{"item":"item"},
+    "result":{"tag":"tag"},
+    "conditions":[
+        {"value":{"tag":"tag","type":"forge:tag_empty"},"type":"forge:not"},
+        {"value":{"tag":"tag","type":"forge:tag_empty"},"type":"forge:not"}
+    ],
+    "input":{
+      "count":value,
+      "base_ingredient":{"tag":"tag"}
+    },
+    "energy":value
+}
