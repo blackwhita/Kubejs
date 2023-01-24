5fe0ab269f8d29b4c1894290d0922438b0526b91
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 18:39:20 +0800

    Create 倒出液体.js

---
+++ 中文/服务端/配方类型/机械动力/倒出液体.js
@@ -0,0 +1,20 @@
+//
+{
+  "type": "create:emptying",
+  "ingredients": [
+    {
+      "item": "item"
+    }
+  ],
+  "results": [
+    {
+      "item": "item"
+    },
+    {
+      "fluid": "fluid",
+      "amount": value
+    }
+  ]
+}
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+event.recipes.createEmptying(output[], input[])
