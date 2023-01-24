326f76eddc7fcb5931986acbee1f35db6bdd89ed
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 21:27:07 +0800

    Update 炼金矩阵.js

--- 中文/服务端/配方类型/血魔法3/炼金矩阵.js
+++ 中文/服务端/配方类型/血魔法3/炼金矩阵.js
@@ -15,3 +15,19 @@ onEvent('recipes', (event) => {
             .id(recipe.id);
     });
 });
+//
+{
+  "type": "bloodmagic:array",
+  "texture": "texture",
+  "baseinput": {
+    "item": "item"
+  },
+  "addedinput": {
+    "item": "item"
+  },
+  "output": {
+    "item": "item"
+  }
+}
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+event.recipes.bloodmagic.array(output, baseInput, addedInput).texture(texture)
