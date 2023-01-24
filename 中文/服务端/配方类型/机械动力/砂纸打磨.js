427cf9b7d6c9f9c5886e82a4393b21da9e4d4420
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 18:49:13 +0800

    Update 砂纸打磨.js

--- 中文/服务端/配方类型/机械动力/砂纸打磨.js
+++ 中文/服务端/配方类型/机械动力/砂纸打磨.js
@@ -10,3 +10,19 @@ onEvent('recipes', (event) => {
         event.recipes.create.sandpaper_polishing(recipe.output, recipe.input).id(recipe.id);
     });
 });
+//
+{
+  "type": "create:sandpaper_polishing",
+  "ingredients": [
+    {
+      "item": "item"
+    }
+  ],
+  "results": [
+    {
+      "item": "item"
+    }
+  ]
+}
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+event.recipes.createSandpaperPolishing(output[], input[])
