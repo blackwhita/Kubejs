91244a797094493b93d706d76813d4686e4e644d
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 19:39:22 +0800

    Create 转化.js

---
+++ 中文/服务端/配方类型/植物魔法/转化.js
@@ -0,0 +1,28 @@
+//
+{
+  "type": "botania:gog_alternation",
+  "gog": {
+    "type": "minecraft:crafting_shapeless",
+    "ingredients": [
+      {
+        "item": "item"
+      }
+    ],
+    "result": {
+      "item": "item",
+      "count": value
+    }
+  },
+  "base": {
+    "type": "minecraft:crafting_shapeless",
+    "ingredients": [
+      {
+        "item": "item"
+      }
+    ],
+    "result": {
+      "item": "item",
+      "count": value
+    }
+  }
+}
