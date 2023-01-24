15ce82a068ebe9285f3624194e88a1f8aaa02748
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 19:16:06 +0800

    Create 粉碎机.js

---
+++ 中文/服务端/配方类型/沉浸工程/粉碎机.js
@@ -0,0 +1,9 @@
+//
+{
+    "type":"immersiveengineering:crusher",
+    "secondaries":[],
+    "result":{"tag":"tag"},
+    "conditions":[{"value":{"tag":"tag","type":"forge:tag_empty"},"type":"forge:not"}],
+    "input":{"item":"item"},
+    "energy":value
+}
