e70628942c57c30cf38c2dd7736604ffcb806b51
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 17:07:26 +0800

    Create 造石机.js

---
+++ 中文/服务端/配方类型/工业先锋/造石机.js
@@ -0,0 +1,11 @@
+{
+  "output": {
+    "item": "item",
+    "count": value
+  },
+  "waterNeed": value,//需要岩浆
+  "lavaNeed": value,//需要水
+  "waterConsume": value,//消耗岩浆
+  "lavaConsume": value,//消耗水
+  "type": "industrialforegoing:stonework_generate"
+}
