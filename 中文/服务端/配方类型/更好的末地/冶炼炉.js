9d8e9536b169f626c95eec1e1e77e94a6ee8b5b7
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 17:46:52 +0800

    Update 冶炼炉.js

--- 中文/服务端/配方类型/更好的末地/冶炼炉.js
+++ 中文/服务端/配方类型/更好的末地/冶炼炉.js
@@ -22,19 +22,4 @@ onEvent('recipes', (event) => {
             .id(recipe.id);
     });
 });
-//
-{
-  "type": "betterendforge:infusion",
-  "input": 
-  {
-     "item": "item"
-  },
-  "output": "item",
-  "time": value,
-  "catalysts": [
-    {
-      "item": "item",
-      "index": 0//0-7
-    }
-  ]
-}
+
