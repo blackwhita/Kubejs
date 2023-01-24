00c788ce0cb09411a351718d6649822ade8ad82a
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 17:59:34 +0800

    Create 燃料.js

---
+++ 中文/服务端/配方类型/气动工艺/燃料.js
@@ -0,0 +1,10 @@
+{
+  "type": "pneumaticcraft:fuel_quality",
+  "fluid": {
+    "type": "pneumaticcraft:fluid",
+    "tag": "fluid",
+    "amount": value
+  },
+  "air_per_bucket": value,
+  "burn_rate": value
+}
