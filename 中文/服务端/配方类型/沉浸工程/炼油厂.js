7129877cd49a316a3aaef58e26c7df15d7aac9ae
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-24 19:29:14 +0800

    Create 炼油厂.js

---
+++ 中文/服务端/配方类型/沉浸工程/炼油厂.js
@@ -0,0 +1,8 @@
+//
+{
+    "type":"immersiveengineering:refinery",
+    "result":{"fluid":"fluid","amount":value},
+    "input0":{"tag":"fluidTag","amount":value},
+    "input1":{"tag":"fluidTag","amount":value},
+    "energy":value
+}
