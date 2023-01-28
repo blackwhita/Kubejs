c76299d200f0cdc9b64a30221752ac7f0674a58a
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-28 14:04:46 +0800

    Update 高炉.js

--- 中文/服务端/配方类型/我的世界/高炉.js
+++ 中文/服务端/配方类型/我的世界/高炉.js
@@ -11,3 +11,12 @@ onEvent('recipes', (event) => {
         event.blasting(recipe.output, recipe.input).xp(recipe.xp).id(recipe.id);
     });
 });
+//
+{
+	"group": "字符串", 可选，具有相同该标识符的配方会在配方书中显示为一组。
+	"type": "minecraft:blasting"
+	"ingredients": "item"
+	"result": "item",
+	"experience": value
+	"cookingtime": value
+}
