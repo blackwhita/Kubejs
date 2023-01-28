77e028d275c6eca44133da28e48767f90a477720
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-28 14:20:20 +0800

    Create 工作台特殊配方.js

---
+++ 中文/服务端/配方类型/我的世界/工作台特殊配方.js
@@ -0,0 +1,28 @@
+//
+{
+	"type": "minecraft:crafting_special_armordye" 盔甲染色配方
+或
+	"type": "minecraft:crafting_special_bannerduplicate" 旗帜复制配方
+或
+	"type": "minecraft:crafting_special_bookcloning" 成书复制配方
+或
+	"type": "minecraft:crafting_special_firework_rocket" 使用烟火之星合成烟花火箭的配方
+或
+	"type": "minecraft:crafting_special_firework_star" 烟火之星的合成配方
+或
+	"type": "minecraft:crafting_special_firework_star_fade" 烟火之星的色彩淡化配方
+或
+	"type": "minecraft:crafting_special_mapcloning" 地图复制配方
+或
+	"type": "minecraft:crafting_special_mapextending" 地图比例缩小配方
+或
+	"type": "minecraft:crafting_special_repairitem" 物品修复配方
+或
+	"type": "minecraft:crafting_special_shielddecoration" 给盾牌添加图案的配方
+或
+	"type": "minecraft:crafting_special_shulkerboxcoloring" 潜影盒染色配方
+或
+	"type": "minecraft:crafting_special_tippedarrow" 药箭配方
+或
+	"type": "minecraft:crafting_special_suspiciousstew" 迷之炖菜配方
+}
