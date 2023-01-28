e371780871d343bfdd5a5cbc1f295c5ecdd03d56
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-28 15:10:36 +0800

    Create event_Tag作用（详细版 ）.js

---
+++ 中文/服务端/Game_events/event_Tag作用（详细版 ）.js
@@ -0,0 +1,56 @@
+<table class="wikitable stikitable">
+<tbody><tr>
+<th>标签名称
+</th>
+<th style="width:40%">值
+</th>
+<th>用途
+</th></tr>
+<tr id="game_events_allay_can_listen">
+<td>allay_can_listen
+</td>
+<td>note_block_play
+</td>
+<td>
+<p>-
+</p><p><br>
+</p>
+</td></tr>
+<tr id="game_events_ignore_vibrations_sneaking">
+<td>ignore_vibrations_sneaking
+</td>
+<td>hit_ground, projectile_shoot, step, swim, item_interact_start, item_interact_finish
+</td>
+<td>
+<ul><li>当玩家潜行时，包含在这个标签里的游戏事件不会被<a href="/mc/%E5%B9%BD%E5%8C%BF%E6%84%9F%E6%B5%8B%E4%BD%93" title="幽匿感测体">幽匿感测体</a>检测。</li></ul>
+<p><br>
+</p>
+</td></tr>
+<tr id="game_events_shrieker_can_listen">
+<td>shrieker_can_listen
+</td>
+<td>sculk_sensor_tendrils_clicking
+</td>
+<td>
+<p>-
+</p><p><br>
+</p>
+</td></tr>
+<tr id="game_events_vibrations">
+<td>vibrations
+</td>
+<td>block_attach, block_change, block_close, block_destroy, block_detach, block_open, block_place, block_activate, block_deactivate, container_close, container_open, dispense_fail, drink, eat, elytra_glide, entity_damage, entity_die, entity_interact, entity_place, entity_roar, entity_shake, equip, explode, fluid_pickup, fluid_place, hit_ground, instrument_play, item_interact_finish, lightning_strike, note_block_play, piston_contract, piston_extend, prime_fuse, projectile_land, projectile_shoot, shear, splash, step, swim, teleport, flap
+</td>
+<td>
+<ul><li>包含在这个标签里的游戏事件发生时，会触发<a href="/mc/%E5%B9%BD%E5%8C%BF%E6%84%9F%E6%B5%8B%E4%BD%93" title="幽匿感测体">幽匿感测体</a>。</li></ul>
+<p><br>
+</p>
+</td></tr>
+<tr id="game_events_warden_can_listen">
+<td>warden_can_listen
+</td>
+<td>block_attach, block_change, block_close, block_destroy, block_detach, block_open, block_place, block_activate, block_deactivate, container_close, container_open, dispense_fail, drink, eat, elytra_glide, entity_damage, entity_die, entity_interact, entity_place, entity_roar, entity_shake, equip, explode, fluid_pickup, fluid_place, hit_ground, instrument_play, item_interact_finish, lightning_strike, note_block_play, piston_contract, piston_extend, prime_fuse, projectile_land, projectile_shoot, shear, splash, step, swim, teleport, shriek, <a href="#game_events_shrieker_can_listen">#shrieker_can_listen</a>
+</td>
+<td>
+<ul><li><a href="/mc/%E7%9B%91%E5%AE%88%E8%80%85" title="监守者"><span class="nowrap"><span class="sprite entity-sprite" style="background-image:url(https://patchwiki.biligame.com/images/mc/4/40/bumw1b6n7ng4vl6b0hup6zi8a7iixei.png?format=original);background-position:-64px -592px"></span><span class="sprite-text">监守者</span></span></a>可以听到的事件。</li></ul>
+</td></tr></tbody></table>
