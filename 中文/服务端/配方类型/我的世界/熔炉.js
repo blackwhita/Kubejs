ea0587f227724143b418929dacda79a5ededff66
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-01-28 14:02:42 +0800

    Update and rename 冶炼1.js to 熔炉.js

--- 中文/服务端/配方类型/我的世界/熔炉.js
+++ 中文/服务端/配方类型/我的世界/熔炉.js
@@ -12,3 +12,12 @@ onEvent('recipes', (event) => {
         event.smelting(recipe.output, recipe.input).xp(recipe.xp).id(recipe.id);
     });
 });
+//
+{
+	"group": "字符串", 可选，具有相同该标识符的配方会在配方书中显示为一组。
+	"type": "minecraft:smelting"
+	"ingredients": "item",
+	"result": "item", 
+	"experience": value
+	"cookingtime": value
+}
