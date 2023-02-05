2af656fb8efe69d62c1f2a34fd9611a4192b3311
Swallow a Minotaur <98508429+blackwhita@users.noreply.github.com>
2023-02-05 12:13:03 +0800

    Update items.js

--- 中文/startup_scripts/custom_item/items.js
+++ 中文/startup_scripts/custom_item/items.js
@@ -1,6 +1,8 @@
 //1.16.5
 //这个是我做的一个创建物品的例子
-event.create('avaritia:akashic_record').displayName('akashic_record').tooltip('by').texture('avaritia:items/akashic_record')
+onEvent('item.registry', event => {
+    event.create('avaritia:akashic_record').displayName('akashic_record').tooltip('by').texture('avaritia:items/akashic_record')
+})
 //其中item代表你在游戏中获取的该物品的id，当然你可以在前面加上模组名例如create("xnet:apple")/create("apple")显示游戏内id分别为xnet:apple和kubejs:apple
 //displayName代表物品在游戏中显示的名称，如果不加可能会出现item.{mod}.{item.name}这种名称,如果你想要在不同语言下用不同名称，请在assets/lang文件夹下做翻译
 //tooltip代表你在物品下面加的提示
